a5cf7fe0251363c3310a1a8c3695b9a245bd9626
Yuii24 <linian2040@gmail.com>
2024-07-29 02:39:39 +0800

    update seeder

--- seeders/20240623073225-initial-data.js
+++ seeders/20240623073225-initial-data.js
@@ -2,37 +2,81 @@
 
 const fs = require("fs");
 const path = require("path")
+const bcrypt = require("bcryptjs")
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    let transaction
+
     try {
+      transaction = await queryInterface.sequelize.transaction()
+
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash("12345678", salt)
+
+
       const dataJ = JSON.parse(fs.readFileSync('E:/AC_C4/restaurant-test/public/restaurant.json', 'utf8'));
       const dataJR = dataJ.results;
-      await queryInterface.bulkInsert('restaurants',
-        Array.from({ length: dataJR.length }).map((_, i) =>
-        ({
-          id: `${dataJR[i].id}`,
-          name: `${dataJR[i].name}`,
-          name_en: `${dataJR[i].name_en}`,
-          category: `${dataJR[i].category}`,
-          image: `${dataJR[i].image}`,
-          location: `${dataJR[i].location}`,
-          phone: `${dataJR[i].phone}`,
-          google_map: `${dataJR[i].google_map}`,
-          description: `${dataJR[i].description}`,
-          rating: `${dataJR[i].rating}`
-        }), {})
-      );
+
+      const restaurants = dataJR.map((restaurant, i) => {
+        let userId = 0;
+        if ([1, 2, 3, 7, 8].includes(i + 1)) { // 检查索引+1是否在指定的数组中
+          userId = 1;
+        } else if ([4, 5, 6].includes(i + 1)) {
+          userId = 2;
+        }
+        return {
+          id: restaurant.id,
+          name: restaurant.name,
+          name_en: restaurant.name_en,
+          category: restaurant.category,
+          image: restaurant.image,
+          location: restaurant.location,
+          phone: restaurant.phone,
+          google_map: restaurant.google_map,
+          description: restaurant.description,
+          rating: restaurant.rating,
+          userId: userId
+        };
+      });
+
+      await queryInterface.bulkInsert("users", [
+        {
+          id: 1,
+          name: "user1",
+          email: "user1@example.com",
+          password: hash,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 2,
+          name: "user2",
+          email: "user2@example.com",
+          password: hash,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ], { transaction })
+
+      console.log('Users inserted');
+
+      await queryInterface.bulkInsert('restaurants', restaurants, { transaction })
       console.log('Data has been successfully inserted.');
+
+      await transaction.commit();
     } catch (error) {
       console.error('Error inserting data:', error);
       throw error; // Re-throw the error to ensure the migration fails
     }
   },
+
+
+
   async down(queryInterface, Sequelize) {
     try {
-      await queryInterface.bulkDelete('restaurants', null, {});
+      await queryInterface.bulkDelete('users', null, {});
       console.log('Data has been successfully deleted.');
     } catch (error) {
       console.error('Error deleting data:', error);
